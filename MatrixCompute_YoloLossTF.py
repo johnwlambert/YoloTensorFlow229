Fixed bugs in mAP calculation; before wasn't denormalizing at test time

MatrixCompute_YoloLossTF.py
@@ -53,7 +53,7 @@ def square_wh(boxes):
   return boxes_wh_squared
 
 
-def compute_iou(box_pred, box_true):
+def compute_iou(box_pred,box_true):
   """
   tensorflow version of computing iou between the predicted box and the gt box
   INPUTS:
@@ -62,39 +62,45 @@ def compute_iou(box_pred, box_true):
   OUTPUTS:
   - iou: Tensor of shape [?,7,7,1], intersection over unit with each predicted bounding box
   """
-  print 'PRED BOX SHAPE: ', box_pred.get_shape().as_list()
-  print 'GT BOX SHAPE: ', box_true.get_shape().as_list()
-  lr = tf.minimum(box_pred[  0] + 0.5 * box_pred[  2],   \
-                  box_true[  0] + 0.5 * box_true[  2]) - \
-       tf.maximum(box_pred[  0] - 0.5 * box_pred[  2],   \
-                  box_true[  0] - 0.5 * box_true[  2])
-  tb = tf.minimum(box_pred[  1] + 0.5 * box_pred[  3],   \
-                  box_true[  1] + 0.5 * box_true[  3]) - \
-       tf.maximum(box_pred[  1] - 0.5 * box_pred[  3],   \
-                  box_true[  1] - 0.5 * box_true[  3])
+  # print 'PRED BOX SHAPE: ', box_pred.get_shape().as_list()
+  # print 'GT BOX SHAPE: ', box_true.get_shape().as_list()
+  lr = tf.minimum(box_pred[ :, 0 ] + 0.5 * box_pred[ :,2 ],   \
+                  box_true[ :, 0 ] + 0.5 * box_true[ :,2 ]) - \
+       tf.maximum(box_pred[ :, 0 ] - 0.5 * box_pred[ :,2 ],   \
+                  box_true[ :, 0 ] - 0.5 * box_true[ :,2 ])
+  tb = tf.minimum(box_pred[ :, 1 ] + 0.5 * box_pred[ :,3 ],   \
+                  box_true[ :, 1 ] + 0.5 * box_true[ :,3 ]) - \
+       tf.maximum(box_pred[ :, 1 ] - 0.5 * box_pred[ :,3 ],   \
+                  box_true[ :, 1 ] - 0.5 * box_true[ :,3 ])
   lr = tf.maximum(lr, lr * 0)
   tb = tf.maximum(tb, tb * 0)
   intersection = tf.mul(tb, lr)
-  union = tf.sub(tf.mul(box_pred[  2], box_pred[ 3]) +  \
-                 tf.mul(box_true[  2], box_true[ 3]), intersection)
+  union = tf.sub(tf.mul(box_pred[ :, 2], box_pred[ :, 3 ]) +  \
+                 tf.mul(box_true[ :, 2], box_true[ :, 3 ]), intersection)
   iou = tf.div(intersection, union)
-  iou = tf.reshape(iou, [NUM_GRID * NUM_GRID, 1])  # otherwise [?, 7, 7]
+
   return iou
 
 
-def compute_ious(pred_boxes, gtbox):
-  """
-  computing ious between two predicted boxes and the ground truth box
-  INPUTS:
-  - pred_boxes: list of TF Tensors of shape [?, 7, 7, 4], xy norm to image, wh as is
-  - gtbox:  Tensor of shape [?, 1, 1, 4], xy norm to image, wh as is
-  OUTPUTS:
-  result: a list of IOUs for each of the pred_boxes with gt box. Shape=[?, 7, 7, NUM_BOX]
-  """
-  ious = [compute_iou(pb, gtbox) for pb in pred_boxes]
-  result = tf.concat(3, ious)
-  return result
+def compute_ious(box_pred,box_true):
+  all_ious = compute_iou(box_pred,box_true)
+  # can reshape if necessary
+  #iou = tf.reshape(iou, [4, 1])  # otherwise [?, 7, 7]
+  return all_ious
 
+
+# def compute_ious(pred_boxes, gtbox):
+#   """
+#   computing ious between two predicted boxes and the ground truth box
+#   INPUTS:
+#   - pred_boxes: list of TF Tensors of shape [?, 7, 7, 4], xy norm to image, wh as is
+#   - gtbox:  Tensor of shape [?, 1, 1, 4], xy norm to image, wh as is
+#   OUTPUTS:
+#   result: a list of IOUs for each of the pred_boxes with gt box. Shape=[?, 7, 7, NUM_BOX]
+#   """
+#   ious = [compute_iou(pb, gtbox) for pb in pred_boxes]
+#   result = tf.concat(3, ious)
+#   return result
 
 
 def computeYoloLossTF( pred_classes, pred_conf, pred_boxes, gt_conf, gt_classes, ind_obj_i, gt_boxes_j0):
@@ -139,12 +145,12 @@ def computeYoloLossTF( pred_classes, pred_conf, pred_boxes, gt_conf, gt_classes,
   squared_pred_boxes_j0 = square_wh(pred_boxes_j0)
   squared_pred_boxes_j1 = square_wh(pred_boxes_j1)
   # For each of the predicted boxes in each grid cell, we check if B_1 or B_2 has a higher IOU with GT
-  pbs_j0 = [pb for pb in tf.split(0, 49, squared_pred_boxes_j0 )] # BREAK INTO chunks of 4
-  ious = compute_ious( pbs_j0, squared_gt_boxes_j0 )
-  pbs_j1 = [pb for pb in tf.split(0, 49, squared_pred_boxes_j1 )] # BREAK INTO chunks of 4
-  temp_ious = compute_ious( pbs_j1, squared_gt_boxes_j0 )
-  mask_temp = tf.greater( temp_ious, ious )
-  final_ious = tf.select(mask_temp, temp_ious, ious )
+  #pbs_j0 = [pb for pb in tf.split(0, 49, squared_pred_boxes_j0 )] # BREAK INTO chunks of 4
+  ious_j0 = compute_ious( squared_pred_boxes_j0 , squared_gt_boxes_j0 )
+  #pbs_j1 = [pb for pb in tf.split(0, 49, squared_pred_boxes_j1 )] # BREAK INTO chunks of 4
+  ious_j1 = compute_ious( squared_pred_boxes_j1 , squared_gt_boxes_j0 )
+  mask_temp = tf.greater( ious_j0, ious_j1 )
+  final_ious = tf.select(mask_temp, ious_j0, ious_j1 )
   j1_coord_loss = tf.reduce_sum(tf.square(pred_boxes_j1 - gt_boxes_j0), reduction_indices=[1] )
   box_loss = tf.select(mask_temp, j0_coord_loss, j1_coord_loss )
   box_loss = LAMBDA_COORD * tf.reduce_sum(box_loss, reduction_indices=[0])
@@ -155,30 +161,48 @@ def computeYoloLossTF( pred_classes, pred_conf, pred_boxes, gt_conf, gt_classes,
   pred_conf_j0 *= gt_conf[:,0]
   pred_conf_j1 *= gt_conf[:,0]
   # Now only one of those predictors is doing the work. Mask out the other
-  j0_mask = tf.logical_and( tf.greater( pred_conf_j0, tf.zeros_like(pred_conf_j0)) )
-  j1_mask = tf.logical_and( tf.logical_not( tf.greater( pred_conf_j1, tf.zeros_like(pred_conf_j1))))
+  j0_mask = tf.logical_and( mask_temp, tf.greater( pred_conf_j0, tf.zeros_like(pred_conf_j0)) )
+  j1_mask = tf.logical_and( tf.greater( pred_conf_j1, tf.zeros_like(pred_conf_j1)), tf.logical_not(mask_temp))
+  
   pred_conf_j0 = tf.select(j0_mask, pred_conf_j0, tf.zeros_like(pred_conf_j0) )
   pred_conf_j1 = tf.select(j1_mask, pred_conf_j1, tf.zeros_like(pred_conf_j1) )
-  j0_obj_loss = tf.square(pred_conf_j0 - gt_conf[:,0])
-  j1_obj_loss = tf.square(pred_conf_j1 - gt_conf[:,0])
-  obj_loss = tf.select(mask_temp, j0_obj_loss, j1_obj_loss )
-  obj_loss = tf.mul( final_ious, obj_loss )
-  obj_loss = tf.reduce_sum(obj_loss, reduction_indices=[0])
+
+  obj_ious = final_ious * gt_conf[:,0]
+  gt_conf_j0 = tf.select(j0_mask, obj_ious, tf.zeros_like(pred_conf_j0) )
+  gt_conf_j1 = tf.select(j1_mask, obj_ious, tf.zeros_like(pred_conf_j0) )
+
+  j0_obj_loss = tf.square( pred_conf_j0 - gt_conf_j0 )
+  j1_obj_loss = tf.square( pred_conf_j1 - gt_conf_j1 )
+
+  obj_loss = tf.reduce_sum( j0_obj_loss + j1_obj_loss, reduction_indices=[0])
   ##############################################################################
 
   ############ NO-OBJECT LOSS ##################################################
-  opposite_mask = tf.logical_not( temp_mask )
-  noobj_loss = tf.select(mask_temp, j0_obj_loss, j1_obj_loss )
-  noobj_loss = tf.mul( noobj_loss, final_ious )
-  noobj_loss = LAMBDA_NOOBJ * tf.reduce_sum(noobj_loss, reduction_indices=[0])
+  gt_conf_noobj_mask = tf.greater( gt_conf, tf.zeros_like(gt_conf) )
+  gt_conf_noobj = tf.select( gt_conf_noobj_mask, tf.zeros_like(gt_conf), tf.ones_like(gt_conf)) # opposite of gt_conf
+
+  j0_mask_noobj = tf.logical_not( j0_mask ) 
+  j1_mask_noobj = tf.logical_not( j1_mask )
+
+  pred_conf_j0_noobj = tf.select(j0_mask_noobj, pred_conf[:,0], tf.zeros_like(pred_conf_j0) )
+  pred_conf_j1_noobj = tf.select(j1_mask_noobj, pred_conf[:,1], tf.zeros_like(pred_conf_j1) )
+
+  noobj_ious = final_ious * gt_conf_noobj[:,0]
+  gt_conf_j0_noobj = tf.select(j0_mask_noobj, noobj_ious, tf.zeros_like(pred_conf_j0) )
+  gt_conf_j1_noobj = tf.select(j1_mask_noobj, noobj_ious, tf.zeros_like(pred_conf_j0) )
+
+  noobj_loss_j0 = tf.square( pred_conf_j0_noobj - gt_conf_j0_noobj )
+  noobj_loss_j1 = tf.square( pred_conf_j1_noobj - gt_conf_j1_noobj )
+
+  noobj_loss = LAMBDA_NOOBJ * tf.reduce_sum(noobj_loss_j0 + noobj_loss_j1, reduction_indices=[0])
   ##############################################################################
 
   ############ COMPLETED CLASS LOSS ############################################
-  masked_pred_classes = tf.mul( ind_obj_i, pred_classes) # both are 49 x 20
-  class_loss = gt_classes - masked_pred_classes
+  class_loss = gt_classes - pred_classes # both are 49 x 20
   class_loss = tf.square(class_loss)
   class_loss = tf.reduce_sum(class_loss, reduction_indices=[1]) # along all classes
+  class_loss = tf.mul( ind_obj_i, class_loss) # both are 49 x 1
   class_loss = tf.reduce_sum(class_loss, reduction_indices=[0]) # along all boxes
   #############################################################################
 
-  return box_loss + obj_loss + noobj_loss + class_loss
+  return box_loss +class_loss #+ noobj_loss#obj_loss # + 

TrainYolo.py
@@ -7,7 +7,7 @@
 # Verify Indicator Probabilities
 # Compute p(c) = p(c | obj) * p(obj) before loss calc
 # Compute mean image, subtract by it, Divide by standard deviation.
-
+import pdb
 from PIL import Image
 import tensorflow as tf
 import math
@@ -75,7 +75,8 @@ def runTrainStep(yoloNet, annotatedImages,sess, step):
 	yoloNet.gt_classes : gt_classes, yoloNet.ind_obj_i: ind_obj_i, \
 	yoloNet.dropout_prob: TRAIN_DROP_PROB, yoloNet.gt_boxes_j0 : gt_boxes_j0 }
 
-	trainLossVal, _ = sess.run( [yoloNet.loss, yoloNet.train_op],feed_dict=feed )
+	trainLossVal = sess.run( yoloNet.loss ,feed_dict=feed ) # yoloNet.train_op
+	#print 'Length of train loss: ', len(trainLossVal)
 	print 'Training loss at step %d: %f' % (step,trainLossVal)
 
 def runEvalStep( splitType, yoloNet, annotatedImages ,sess, epoch, saver, best_val_mAP ):
@@ -90,8 +91,12 @@ def runEvalStep( splitType, yoloNet, annotatedImages ,sess, epoch, saver, best_v
 	"""
 	detections = []
 	print '====> Evaluating: %s at epoch %d =====>' % (splitType, epoch)
-	for i in range( data_set_size ):
-		minibatchIms, minibatchGTs = sampleMinibatch(annotatedImages, plot_yolo_grid_cells, plot_bbox_centerpoints)
+	if splitType == 'val':
+		data_set_size = VAL_SET_SIZE
+	elif splitType == 'test':
+		data_set_size = TEST_SET_SIZE
+	for i in range( 500): #data_set_size ):
+		minibatchIms, minibatchGT = sampleMinibatch(annotatedImages, plot_yolo_grid_cells, plot_bbox_centerpoints)
 		minibatchIms = np.expand_dims( minibatchIms, 0)
 
 		gt_conf,gt_classes,ind_obj_i,gt_boxes_j0 = minibatchGT
@@ -105,35 +110,55 @@ def runEvalStep( splitType, yoloNet, annotatedImages ,sess, epoch, saver, best_v
 		yoloNet.gt_classes : gt_classes, yoloNet.ind_obj_i: ind_obj_i, \
 		yoloNet.dropout_prob: TEST_DROP_PROB, yoloNet.gt_boxes_j0 : gt_boxes_j0 }
 
-		class_probs_giv_obj,confidences,bboxes = sess.run( [self.class_probs,self.confidences,self.bboxes],feed_dict=feed )
-
+		class_probs_giv_obj,confidences,boxes, lossVal = sess.run( [yoloNet.class_probs,yoloNet.confidences,yoloNet.bboxes, yoloNet.loss],feed_dict=feed )
+		print 'Loss in %s split at epoch%d , iter %d : %f' % (splitType, epoch, i, lossVal)
 		# NOW PROCESS THE PREDICTIONS HERE
 		boxes = np.reshape(boxes, [NUM_GRID*NUM_GRID,NUM_BOX,4] )
-		boxes = convertWH_to_xMax_yMax( boxes )
+		boxes = unnormalizeBoxes(boxes, minibatchIms)
+		gt_boxes_j0 = unnormalizeGTBoxes(gt_boxes_j0,minibatchIms)
+		#boxes = convertWH_to_xMax_yMax( boxes )
 		class_probs = np.zeros((NUM_GRID*NUM_GRID,NUM_BOX,NUM_CLASSES))
 		# We use a law of probability: prob(class) = prob(class|object) * prob(object)
 														# 49 x 20 			49 x 2
 		for i in range(NUM_BOX):
 			for j in range(NUM_CLASSES):
-				class_probs[:,i,j] = np.multiply(class_probs_giv_obj[:,:,j],confidences[:,:,i])
+				class_probs[:,i,j] = np.multiply(class_probs_giv_obj[:,j],confidences[:,i])
 
-		class_indices = np.argmax( class_probs, axis = 2)
+		class_probs = np.reshape( class_probs, [-1,20] )
+		confidences = np.reshape( confidences, [-1,1] )
+		boxes = np.reshape( boxes, [-1,4] )
+
+		class_indices = np.argmax( class_probs, axis = 1)
 		valid_detection_indices = np.where( confidences > 0.2 )[0]
 
 		class_indices = class_indices[valid_detection_indices]
 		confidences = confidences[valid_detection_indices]
 		boxes = boxes[valid_detection_indices,:]
-		print 'Class Indices have shape: ', class_indices.shape
-		
+		#print 'Class Indices have shape: ', class_indices.shape
+		# pdb.set_trace()
+		# gt_nonzero_indices = []
+		# for gridIdx in range(49):
+		# 	if np.sum(gt_boxes_j0[gridIdx]) > 0.9:
+		# 		gt_nonzero_indices.append( gridIdx)
+		# gt_nonzero_indices = np.asarray( gt_nonzero_indices )
+		# print 'NON_ZERO_INDICES: ', gt_nonzero_indices
+		gt_nonzero_indices = np.where( np.sum(gt_classes, axis = 1) > 0.9 )[0]
+		gt_boxes_j0 = gt_boxes_j0[gt_nonzero_indices]
+		#print 'GT_classes looks like: ', gt_classes
+		gt_classes = gt_classes[gt_nonzero_indices]
+		#print 'Shrunken gt_classes: ', gt_classes
+		gt_classes = np.argmax( gt_classes, axis = 1)
+		#print 'argmaxed gt_classes: ', gt_classes
+
 		detections.append( { 'pred_classes':class_indices, 'confidences':confidences,\
-			'bboxes':boxes, 'gt_boxes_j0':gt_boxes_j0, 'gt_classes': gt_classes   })
+			'bboxes':boxes, 'gt_boxes_j0':gt_boxes_j0, 'gt_classes': gt_classes, 'im':minibatchIms   })
 
 	# BB, BBGT = convertPredsAndGTs(bboxes, class_probs, confidences )
 	mAP = computeMeanAveragePrecision(detections, splitType)
 	# save some of the plots just as a sanity check along the way
-
+	print '==> Current mAP: ', mAP, ' ===>'
 	if (splitType == 'val') and (mAP > best_val_mAP):
-		saver.save(session, './%s/YOLO_Trained.weights' % (new_dir_path ) )
+		#saver.save(sess, './YOLO_Trained.weights' )
 		best_val_mAP = mAP
 	# plot_detections_on_im( imread(self.image_path),probs,confidences,bboxes,classes)
 
@@ -142,21 +167,86 @@ def runEvalStep( splitType, yoloNet, annotatedImages ,sess, epoch, saver, best_v
 	# Why not just at test time do cross product?
 
 
-def convertWH_to_xMax_yMax( boxes ):
+def unnormalizeBoxes(boxes, im):
 	"""
 	INPUTS:
-	-	boxes: n-d array contains [xmin,ymin,w,h] columns
+	-	boxes: NUM_GRID*NUM_GRID,NUM_BOX,4
 	OUTPUTS:
-	-	boxes: n-d array contains [xmin,ymin,xmax,ymax] columns
+	-	boxes: but scaled to image, and 
+
+		x_cent,y_cent,w,h
 	"""
-	xmin = boxes[:,:,0]
-	ymin = boxes[:,:,1]
-	boxes[:,:,2] += xmin # xmin + w = xmax
-	boxes[:,:,3] += ymin # ymin + h = ymax
+	im = np.squeeze( im )
+	imWidth = im.shape[1]
+	imHeight = im.shape[0]
+	grid_width = int(np.floor(imWidth / 7.))
+	grid_height = int(np.floor(imHeight / 7.))
+
+	for row in range(7):
+		for col in range(7):
+			gridIdx = (row*7) + col
+			grid_x_offset = col * grid_width
+			grid_y_offset = row * grid_height
+			for boxIdx in range(2):
+				# box_x_offset = boxes[gridIdx,j,0] * grid_width
+				# box_y_offset = boxes[gridIdx,j,1] * grid_height
+				# boxes[gridIdx,j,0] = box_x_offset + grid_x_offset
+				# boxes[gridIdx,j,1] = box_y_offset + grid_y_offset
+				# box_sqrt_w
+				x_cent = (boxes[gridIdx,boxIdx,0] + col) / 7.0 * imWidth
+				y_cent = (boxes[gridIdx,boxIdx,1] + row) / 7.0 * imHeight
+				w = (boxes[gridIdx,boxIdx,2]**2) * imWidth * 1.0
+				h = (boxes[gridIdx,boxIdx,3]**2) * imHeight * 1.0
+				xmin = max(0, x_cent - w/2. )
+				xmax = min( x_cent + w/2., imWidth-1 )
+				ymin = max(0, y_cent - h/2. )
+				ymax = min( y_cent + h/2., imHeight-1 )
+				boxes[gridIdx,boxIdx,:] = np.array([xmin,ymin,xmax,ymax])
+	# now have xcent,ycent,w,h
+	# convert to xmin,xmax,ymin,ymax
+
 	return boxes
 
 
+def unnormalizeGTBoxes(boxes,im):
+	im = np.squeeze( im )
+	imWidth = im.shape[1]
+	imHeight = im.shape[0]
+	grid_width = int(np.floor(imWidth / 7.))
+	grid_height = int(np.floor(imHeight / 7.))
+
+	for row in range(7):
+		for col in range(7):
+			gridIdx = (row*7) + col
+			x_cent = (boxes[gridIdx,0] + col) / 7.0 * imWidth
+			y_cent = (boxes[gridIdx,1] + row) / 7.0 * imHeight
+			w = (boxes[gridIdx,2]**2) * imWidth * 1.0
+			h = (boxes[gridIdx,3]**2) * imHeight * 1.0
+			xmin = max(0, x_cent - w/2. )
+			xmax = min( x_cent + w/2., imWidth-1 )
+			ymin = max(0, y_cent - h/2. )
+			ymax = min( y_cent + h/2., imHeight-1 )
+			boxes[gridIdx,:] = np.array([xmin,ymin,xmax,ymax])
+	return boxes
+
+# def convertWH_to_xMax_yMax( boxes ):
+# 	"""
+# 	INPUTS:
+# 	-	boxes: n-d array contains [xmin,ymin,w,h] columns
+# 	OUTPUTS:
+# 	-	boxes: n-d array contains [xmin,ymin,xmax,ymax] columns
+# 	"""
+# 	xmin = boxes[:,:,0]
+# 	ymin = boxes[:,:,1]
+# 	boxes[:,:,2] += xmin # xmin + w = xmax
+# 	boxes[:,:,3] += ymin # ymin + h = ymax
+# 	return boxes
+
+
 if __name__ == '__main__':
+
+	checkpoint_path = '/Users/johnlambert/Documents/Stanford_2016-2017/CS_229/229CourseProject/YoloTensorFlow229/yolo.ckpt'
+
 	best_val_mAP = -1 * float('inf')
 	annotatedImages = getData(getPickledData,vocImagesPklFilename)
 	trainData, valData, testData = separateDataSets(annotatedImages)
@@ -164,16 +254,24 @@ if __name__ == '__main__':
 		plotGroundTruth(annotatedImages)
 	yoloNet = YOLO_TrainingNetwork( use_pretrained_weights = False)
 	numItersPerEpoch = TRAIN_SET_SIZE / BATCH_SIZE
-	saver = tf.train.Saver()
+
+	# t = 1
+	# beta1 = tf.convert_to_tensor(0.9)
+	# beta2 = tf.convert_to_tensor(0.999)
+	# beta1_power = beta1**t
+	# beta2_power = beta2**t
+
+	saver = tf.train.Saver() # {'beta2_power':beta2_power} )
 	with tf.Session() as sess:
 		sess.run(tf.initialize_all_variables())
-		for epoch in range(NUM_EPOCHS):
+		saver.restore(sess, checkpoint_path)
+		for epoch in range(20):#NUM_EPOCHS):
 			print '====> Starting Epoch %d =====>' % (epoch)
-			for step in range(numItersPerEpoch):
+			for step in range( 1): # numItersPerEpoch):
 				runTrainStep(yoloNet, annotatedImages ,sess, step)
 			runEvalStep( 'val', yoloNet, annotatedImages ,sess, epoch, saver, best_val_mAP)
 		# After all training complete
-		saver.restore
+		saver.restore(sess, './YOLO_Trained.weights' )
 		runEvalStep( 'test', yoloNet, annotatedImages ,sess, epoch, None, None)
 
 

YOLO_DataUtils.py
@@ -9,8 +9,9 @@ from scipy.misc import imread, imresize
 
 from YOLO_PlottingUtils import *
 from YOLO_CoverageMap import *
-
+from preprocess_data import *
 ###### HYPERPARAMETERS #######
+voc_data_path = 'home/johnwl/YoloTensorFlow229/VOCdevkit/'
 PERCENT_TRAIN_SET = 0.8
 PERCENT_VAL_SET = 0.1
 PERCENT_TEST_SET = 0.1
@@ -89,7 +90,7 @@ def sampleMinibatch(annotatedImages, plot_yolo_grid_cells, plot_bbox_centerpoint
 
 	gt_classes = np.zeros((49,20))
 	gt_conf = np.zeros((49,4))
-	ind_obj_i = np.zeros((49,20))
+	ind_obj_i = np.zeros((49))
 	gt_boxes_j0 = np.zeros((49,4))
 	
 	im = imread(image_path)
@@ -119,8 +120,8 @@ def sampleMinibatch(annotatedImages, plot_yolo_grid_cells, plot_bbox_centerpoint
 			j = 0 # 2nd box slot for this grid cell
 			gt_classes[gridCellRow * 7 + gridCellCol, classIdx ] = 1
 			xywh = np.array([ normalizedXCent, normalizedYCent, math.sqrt(normalizedW), math.sqrt(normalizedH) ])
-			bboxGT = np.hstack(( classGTs , xywh, coverageMap)) # coverage map is the confidence
-			gt_boxes_j0[ gridCellRow * 7 + gridCellCol, bboxGT]
+			bboxGT = xywh # coverage map is the confidence
+			gt_boxes_j0[ gridCellRow * 7 + gridCellCol] = bboxGT
 			occupiedSlot[gridCellRow,gridCellCol,j] = CONTAINS_IMAGE_FLAG
 
 			# values in each of 4 columns are identical (tiled/repmatted). Object here at cell i!
@@ -135,8 +136,8 @@ def sampleMinibatch(annotatedImages, plot_yolo_grid_cells, plot_bbox_centerpoint
 		# 	gt = np.hstack(( classGTs , xywh, coverageMap)) # coverage map is the confidence
 		# 	occupiedSlot[gridCellRow,gridCellCol,j] = CONTAINS_IMAGE_FLAG
 		else:
-			print 'In Image %d, no more room in some grid cell for this bbox.' % (imNum)
-
+			#print 'In Image %d, no more room in some grid cell for this bbox.' % (imNum)
+			pass
 	if plot_bbox_centerpoints == True:
 		plt.scatter(x_cent,y_cent)
 	if plot_bbox_centerpoints or plot_yolo_grid_cells:

YOLO_TrainingNetwork.py
@@ -8,6 +8,7 @@ from functools import reduce
 ########################################################
 NUM_CLASSES = 20
 NUM_GRID = 7
+LEARNING_RATE = 1e-4
 ########################################################
 START_IDX_PROBS = 0
 END_IDX_PROBS = (NUM_CLASSES) * (NUM_GRID**2)
@@ -21,13 +22,14 @@ END_IDX_BBOXES = START_IDX_BBOXES + (NUM_GRID**2)*2*4
 from MatrixCompute_YoloLossTF import *
 
 class YOLO_TrainingNetwork:
-    def __init__(self, use_pretrained_weights):
+    def __init__(self,  use_pretrained_weights):
+        #self.weight_path = weight_path
         self.num_classes = 20
         self.S = 7
         self.B = 2
         self.use_open_cv = False
         self.add_placeholders()
-        self.lr = 1e-4
+        self.lr = LEARNING_RATE
         self.pretrained_weights = use_pretrained_weights
         self.create_network()
 
@@ -102,7 +104,8 @@ class YOLO_TrainingNetwork:
         #pred_boxes_arr = pred_labels[:, :, :, NUM_CLASSES : NUM_CLASSES + NUM_BOX * 4]
 
         self.loss = computeYoloLossTF( self.class_probs, self.confidences, self.bboxes, self.gt_conf,self.gt_classes, self.ind_obj_i, self.gt_boxes_j0)
-        self.train_op = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
+        #self.train_op = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
+        self.train_op = tf.train.GradientDescentOptimizer(self.lr).minimize(self.loss)
 
         # tf.contrib.metrics.streaming_sparse_average_precision_at_k(predictions, labels, k, weights=None, metrics_collections=None, updates_collections=None, name=None)
 

YOLO_mAP_Evaluation.py
@@ -3,8 +3,10 @@
 
 import os
 import numpy as np
+import pdb
+import matplotlib.pyplot as plt
 
-
+CLASSES = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
 
 EPSILON = 1e-5
 
@@ -18,11 +20,16 @@ def computeMeanAveragePrecision(detections, splitType):
 	This score corresponds to the area under the precision-recall curve.
 	The mean of these numbers is the mAP.
 	"""
+
+	#plotDets(detections)
 	aps = []
-	for i, cls in enumerate(CLASSES):
-	    rec, prec, ap = matchGTsAndComputePrecRecallAP(cls,detections,ovthresh=0.5)
-	    aps += [ap]
-	    print('AP for {} = {:.4f}'.format(cls, ap))
+	for classIdx, cls in enumerate(CLASSES):
+		pdb.set_trace()
+		#print 'Evaluate class: ', cls, ' with class index: ', classIdx
+		rec, prec, ap = matchGTsAndComputePrecRecallAP(classIdx,detections,iouthresh=0.5)
+		if ap is not None:
+			aps += [ap]
+			print('AP for {} = {:.4f}'.format(cls, ap))
 	print('Mean AP = {:.4f}'.format(np.mean(aps)))
 	print('~~~~~~~~')
 	print('Results:')
@@ -75,7 +82,7 @@ def intersection(BBGT,bb):
 	Why do they inflate numbers by adding +1.? I don't
 	https://github.com/rbgirshick/py-faster-rcnn/blob/master/lib/datasets/voc_eval.py#L168
 	"""
-	print BBGT[ 0], bb[0]
+	#print BBGT[ 0], bb[0]
 	ixmin = np.maximum(BBGT[ 0], bb[0])
 	iymin = np.maximum(BBGT[ 1], bb[1])
 	ixmax = np.minimum(BBGT[ 2], bb[2])
@@ -103,11 +110,12 @@ def union(BBGT,bb,inters):
 def computeIOU(BBGT,bb): 
 	maxIOU = 0
 	for i,gtBbox in enumerate(BBGT):
-		print gtBbox
+		gtBbox = np.squeeze(gtBbox)
+		#print gtBbox
 		inters = intersection(gtBbox,bb) * 1.0
-		print 'Intersection: ', inters
+		#print 'Intersection: ', inters
 		uni = union(gtBbox,bb,inters)
-		print 'Union: ', uni
+		#print 'Union: ', uni
 		iou =   inters/uni
 		maxIOU = max(iou,maxIOU)
 	return maxIOU
@@ -117,6 +125,56 @@ def unitTestAP():
 	prec = np.array([1.,1.,.67,.75,.6,0.67,4/7., .5, 4/9., 0.5])
 	rec = np.array([.2,.4,.4,.6,.6,.8,.8,.8,.8,1.])
 	assert (voc_ap(rec, prec) - naiveAPCalculation(rec,prec)) < EPSILON
+
+def plotRectangle(bbox,ax,class_name, edgecolor):
+	"""
+	"""
+	xmin = bbox[0]
+	ymin = bbox[1]
+	xmax = bbox[2]
+	ymax = bbox[3]
+	left = xmin
+	right = xmax
+	top = ymin
+	bot = ymax
+	ax.add_patch(
+        plt.Rectangle((left, top),
+                      right-left,
+                      bot-top, fill=False,
+                      edgecolor=edgecolor, linewidth=3.5)
+        )
+	ax.text(left, top-2,
+            '{:s}'.format(class_name ),
+            bbox=dict(facecolor='blue', alpha=0.5),
+            fontsize=14, color='white')
+
+
+def plotDets(detections):
+
+	for imIdx,im in enumerate(detections):
+
+		image = im['im']
+		image = np.squeeze(image)
+		imWidth = image.shape[1]
+		imHeight = image.shape[0]
+		fig, ax = plt.subplots(figsize=(8, 8))
+		ax.imshow(image, aspect='equal')
+
+		# look at each of the predictions
+		for j in range( len(im['bboxes'])):
+			pred_class = im['pred_classes'][j]
+			bbox = im['bboxes'][j] 
+			plotRectangle(bbox,ax,CLASSES[pred_class], 'red')
+		for j in range( len(im['gt_boxes_j0'])):
+			gt_class = im['gt_classes'][j]
+    		gt_bbox = im['gt_boxes_j0'][j]
+    		plotRectangle(bbox,ax,CLASSES[gt_class], 'green')
+
+		plt.draw()
+		plt.tight_layout()
+		#plt.show()
+		plt.savefig( 'Image_%d.png' % imIdx)
+		plt.gcf().set_size_inches(15, 12)
 
 
 def matchGTsAndComputePrecRecallAP(cls,detections,iouthresh=0.5):
@@ -136,11 +194,10 @@ def matchGTsAndComputePrecRecallAP(cls,detections,iouthresh=0.5):
 	"""
 	BBGT = []
 	confidence = []
-	predBB = []
+	BB = []
 	BB_im_ids = []
 
 	imIdToNumGtInsideImDict = {}
-
 	# FIND THE PREDICTIONS FOR JUST --ONE CLASS--, E.G. AIRPLANE
 	for imIdx,im in enumerate(detections):
 		# look at each of the predictions
@@ -148,53 +205,70 @@ def matchGTsAndComputePrecRecallAP(cls,detections,iouthresh=0.5):
 			if im['pred_classes'][j] == cls:
 				BB.append( im['bboxes'][j] )
 				confidence.append( im['confidences'][j] )
-				im_ids.append( imIdx )
+				BB_im_ids.append( imIdx )
 		for j in range( len(im['gt_boxes_j0'])):
 			num_of_this_class_in_this_im = 0
     		if im['gt_classes'][j] == cls:
     			BBGT.append( im['gt_boxes_j0'][j] )
     			num_of_this_class_in_this_im += 1
     		imIdToNumGtInsideImDict[imIdx] = num_of_this_class_in_this_im
-
 	BBGT = np.asarray(BBGT)
+	if BBGT.shape[0] == 0:
+		return None,None,None
 	confidence = np.asarray(confidence)
 	BB = np.asarray(BB)
+	if BB.shape[0] > 0:
+		print 'At least one detection made.'
+	#print 'BBGT has shape: ', BBGT.shape
+	BB_im_ids = np.asarray(BB_im_ids)
 
 	# sort by confidence
 	sorted_ind = np.argsort(confidence) # sort according to highest confidence
 	sorted_scores = np.sort(confidence) # sort according to highest confidence
-	BB = BB[sorted_ind, :]
-	BB_im_ids = BB_im_ids[sorted_ind,:]
 
-	#get k highest rank (confidence score) things
-	for k in range( BB.shape[0] ):
-		# then choose next most confident thing
-		#get unique imIds inside current rank list
-		total_cls_reports = k
-		uniqueImIds = np.unique( BB_im_ids[:k])
-		for uniqueImId in uniqueImIds:
-			total_cls_groundtruth += imIdToNumGtInsideImDict[uniqueImId]
-
-		# go down detections and mark TPs and FPs
-		fp = 0
-		tp = 0
-		if BBGT_classes == BB_classes:
-			bb = BB[d, :].astype(float)
-			ovmax = -np.inf
-			if BBGT.size > 0:
+	prec = []
+	rec = []
+	if BB.shape[0] > 0:
+		print 'One Valid Detection.'
+		#pdb.set_trace()
+		BB = BB[sorted_ind]
+		BB_im_ids = BB_im_ids[sorted_ind]
+		#print 'BB.shape: ', BB.shape
+		BB = np.squeeze(BB,axis=1)
+		#print 'BB.shape: ', BB.shape
+		BB_im_ids = np.squeeze(BB_im_ids, axis=1)
+		#get k highest rank (confidence score) things
+		for k in range( 1,BB.shape[0]+1 ):
+			# then choose next most confident thing
+			#get unique imIds inside current rank list
+			total_cls_reports = k
+			#print 'BB_im_ids has shape: ', BB_im_ids.shape
+			uniqueImIds = np.unique( BB_im_ids[:k])
+			total_cls_groundtruth = 0
+			for uniqueImId in uniqueImIds:
+				total_cls_groundtruth += imIdToNumGtInsideImDict[uniqueImId]
+			# go down detections and mark TPs and FPs
+			fp = 0
+			tp = 0
+			for j in range( k ):
+				bb = BB[j, :].astype(float)
+				bb = np.squeeze(bb)
 				iou = computeIOU(BBGT,bb)
+				print 'IOU: ', iou
 		        if iou > iouthresh:
-		            if BBGT_classes[d] == BB_classes[d]:
-		                tp += 1.
-		            else:
-		                fp += 1.
+		        	tp += 1.
 		        else:
-		            fp += 1.
+		        	fp += 1.
+			# compute precision recall
+			prec.append( tp / max(total_cls_reports, np.finfo(np.float64).eps)) # how accurate are reports
+			rec.append( tp / max(total_cls_groundtruth, np.finfo(np.float64).eps)) # how many ground truth can be found by the algorithm
+	else:
+		# no detections
+		rec = [0]
+		prec = [0]
 
-	# compute precision recall
-	precision = tp / maximum(tp + fp, np.finfo(np.float64).eps) # how accurate are reports
-	recall = tp / maximum(tp + fn, np.finfo(np.float64).eps) # how many ground truth can be found by the algorithm
-
+	print 'Recall: ', rec
+	print 'Precision: ', prec
 	# avoid divide by zero
 	ap = voc_ap(rec, prec )
 	# My implementation vs. Ross Girshick's implementation -- should be same area under curve