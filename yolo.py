1bf43059b37c8a1ed4be0fa0bb2f5d79ba881fb3
Matthew Vilim <mvilim@stanford.edu>
2017-04-26 12:24:19 -0700

    add K-means grouping

--- yolo.py
+++ yolo.py
@@ -189,24 +189,72 @@ class YOLO:
         h = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
         out = cv2.VideoWriter('data/output.avi', cv2.cv.CV_FOURCC(*'XVID'), 20.0, (w, h))
 
-        while cap.isOpened():
+        i = 0
+        frame_window = []
+        while cap.isOpened() and i < end_frame:
             ret, frame = cap.read()
 
-            frame_out, bounding_boxes = self.process_image(frame)
-            frame_detections.append(bounding_boxes)
-            for box in bounding_boxes:
-                print box.category
-            #cv2.imwrite('data/' + str(i) + '.png', frame_out)
-            out.write(frame_out)
+            if i >= start_frame:
+                print "processing frame " + str(i)
+                frame_out, bounding_boxes = self.process_image(frame)
+                frame_window.append(bounding_boxes)
+                # number of con
+                window_size = 3
+                if len(frame_window) > 3:
+                    frame_window.pop(0)
+                #if len(frame_window) == 3:
+                #    self.group_cropped(frame_window, len(bounding_boxes))
+                #frame_detections.append(bounding_boxes)
+                #for box in bounding_boxes:
+                #    print box.category
+                #cv2.imwrite('data/' + str(i) + '.png', frame_out)
+                out.write(frame_out)
+            i = i + 1
         cap.release()
         out.release()
+
+    def group_cropped(self, frame_window, group_count):
+        # concatenate all objects across all frames within the frame window
+        x = reduce(list.__add__, frame_window)
+        size = frame_window[0][0].img.shape
+        centroids = []
+        for i in xrange(0, group_count):
+            #centroids.append(np.random.randint(255, size=size))
+            centroids.append(frame_window[0][i].img)
+        c_old = np.zeros(len(x))
+        while True:
+            c = np.zeros(len(x))
+            for i, x_i in enumerate(x):
+                c_min = float('inf')
+                for j, centroid in enumerate(centroids):
+                    c_temp = np.square(np.linalg.norm(x_i.img - centroid, 2))
+                    if c_temp < c_min:
+                        c[i] = j
+                        c_min = c_temp
+            # update centroids
+            for j, centroid in enumerate(centroids):
+                numerator = np.zeros(size)
+                denominator = 0.0
+                for i, c_i in enumerate(c):
+                    if (c_i == j):
+                        numerator = numerator + x[i].img
+                        denominator = denominator + 1.0
+                centroids[j] = numerator / denominator
+            # break after convergence
+            if (c == c_old).all():
+                break
+            else:
+                c_old = c
+        print c_old
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-w', '--weight_path', help='path to weights/biases for each layer')
     parser.add_argument('-t', '--image_path', help='path to test image')
-    parser.add_argument('-v', '--video_path', help='path to test video')
     parser.add_argument('-c', '--checkpoint_path', help='path to create checkpoint', required=True)
+    parser.add_argument('-v', '--video_path', help='path to test video')
+    parser.add_argument('-s', '--start', help='video start frame')
+    parser.add_argument('-e', '--end', help='video end frame')
     args = parser.parse_args()
 
     checkpoint_path = os.path.abspath(os.path.expanduser(args.checkpoint_path))
@@ -220,7 +268,7 @@ def main():
         cv2.imwrite('data/out.png', img)
     if args.video_path:
         video_path = os.path.abspath(os.path.expanduser(args.video_path))
-        yolo.process_video(video_path)
+        yolo.process_video(video_path, int(args.start), int(args.end))
 
 if __name__ == "__main__":
     main()
